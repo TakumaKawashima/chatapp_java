eb5d6a68895128ec687cb7154ca4c62082dc39ac
TakumaKawashima <67384584+TakumaKawashima@users.noreply.github.com>
2024-10-22 15:29:00 +0900

    initial commit

---
+++ src/main/java/in/tech_camp/chatapp/controller/MessageController.java
@@ -0,0 +1,12 @@
+package in.tech_camp.chatapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+  @GetMapping("/")
+  public String Index() {
+    return "messages/index";
+  }
+}
