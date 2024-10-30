e53910ad5fa53cc0cb9ccbf4065db496aea07952
TakumaKawashima <67384584+TakumaKawashima@users.noreply.github.com>
2024-10-30 13:24:22 +0900

    ルーム新規投稿画面の作成＆表示

--- src/main/java/in/tech_camp/chatapp/controller/RoomsController.java
+++ src/main/java/in/tech_camp/chatapp/controller/RoomsController.java
@@ -1,13 +1,24 @@
 package in.tech_camp.chatapp.controller;
 
+import in.tech_camp.chatapp.form.RoomForm;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomsController {
 
+  // ルーム一覧画面の表示
   @GetMapping("/rooms")
   public String index() {
     return "rooms/index"; // ビューの名前
+  }
+
+  // ルーム新規作成画面の表示
+  @GetMapping("/roomForm")
+  public String roomForm(Model model) {
+    model.addAttribute("roomForm", new RoomForm());
+    return "rooms/roomForm"; // ビューの名前
   }
 }
